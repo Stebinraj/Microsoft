2b7fda77d6f63a5b844ab69a47f0e52281c741e0
Stebinraj <stebinraj22@gmail.com>
2024-03-31 20:47:57 +0530

    ...

--- Search-Component/src/App.jsx
+++ Search-Component/src/App.jsx
@@ -8,35 +8,35 @@ const App = () => {
 				style={{ backgroundColor: "#f1f7fa" }}
 			>
 				<div className="software">
-					<label htmlFor className="form-label">
+					<label htmlFor="" className="form-label">
 						How is the software acquired?
 						<span className="text-danger">*</span>
 					</label>
-					<select name id className="form-select p-1">
+					<select name="" id="" className="form-select p-1">
 						<option value="select">Select</option>
 					</select>
 				</div>
 				<div className="product">
-					<label htmlFor className="form-label">
+					<label htmlFor="" className="form-label">
 						Product Name
 					</label>
-					<select name id className="form-select p-1">
+					<select name="" id="" className="form-select p-1">
 						<option value="select">Select</option>
 					</select>
 				</div>
 				<div className="version">
-					<label htmlFor className="form-label">
+					<label htmlFor="" className="form-label">
 						Version
 					</label>
-					<select name id className="form-select p-1">
+					<select name="" id="" className="form-select p-1">
 						<option value="select">Select</option>
 					</select>
 				</div>
 				<div className="language">
-					<label htmlFor className="form-label">
+					<label htmlFor="" className="form-label">
 						Language:
 					</label>
-					<select name id className="form-select p-1">
+					<select name="" id="" className="form-select p-1">
 						<option value="select">Select</option>
 					</select>
 				</div>
@@ -52,7 +52,7 @@ const App = () => {
 					<h5>OR</h5>
 				</div>
 				<div className="manual-search">
-					<label htmlFor>Search For :</label>
+					<label htmlFor="">Search For :</label>
 					<input type="text" className="form-control p-1" />
 				</div>
 				<div className="manual-submit-btn">
